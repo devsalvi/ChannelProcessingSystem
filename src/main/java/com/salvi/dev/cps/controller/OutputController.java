84373d105bf7829da48409a371a02b83a0679f43
dev <dev.salvi@gmail.com>
2024-09-09 22:01:04 -0400

    error handling on page

--- src/main/java/com/salvi/dev/cps/controller/OutputController.java
+++ src/main/java/com/salvi/dev/cps/controller/OutputController.java
@@ -11,11 +11,13 @@ import java.util.stream.Collectors;
 import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -32,14 +34,14 @@ public class OutputController {
     @GetMapping("/files/{filename:.+}")
     @ResponseBody
     public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
-                return ResponseEntity.ok().build();
+        return ResponseEntity.ok().build();
     }
 
     @PostMapping("/")
     public String handleFileUpload(@RequestParam("channels") MultipartFile channelsFile,
             @RequestParam("parameters") MultipartFile parametersFile,
             RedirectAttributes redirectAttributes) {
-                Metric outpuMetric = new Metric();
+        Metric outpuMetric = new Metric();
 
         try {
             // Process channels.txt
@@ -54,11 +56,31 @@ public class OutputController {
             MBAMGApplication mbamgApplication = new MBAMGApplication();
             outpuMetric = mbamgApplication.getMetric(channelLines, paramLines);
 
-        } catch (IOException e) {
+        } catch (IOException | MaxUploadSizeExceededException e) {
             e.printStackTrace();
         }
         redirectAttributes.addFlashAttribute("message",
                 "Results for metric b: " + outpuMetric.getB());
         return "redirect:/";
+    }
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseEntity maxUploadSizeExceeded(MaxUploadSizeExceededException e) {
+        return ResponseEntity.badRequest().body("File size limit exceeded");
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity illegalFileCompositions(IllegalArgumentException e) {
+        return ResponseEntity.badRequest().body("Error: " + e.getMessage());
+    }
+
+    @ExceptionHandler(IndexOutOfBoundsException.class)
+    public ResponseEntity errorProcessingfiles(IndexOutOfBoundsException e) {
+        return ResponseEntity.badRequest().body("Error processing files");
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity runtimeErrorProcessingfiles(RuntimeException e) {
+        return ResponseEntity.badRequest().body("Runtime Error processing files");
     }
 }
